d658ac9bc3037e35ca8265ce38c39b3000d1576e
Theophillus-Keitumetse <mk24019719@biust.ac.bw>
2026-08-30 19:33:43 +0200

    Add question1 - Personal profile

---
+++ Question1.java
@@ -0,0 +1,17 @@
+public class Question1 {
+    public static void main(String[] args) {
+        
+        //Declaration of variables//
+        String name = "Keitumetse Mahempe";
+        int age = 20;
+        double gpa = 3.0;
+        boolean fullTime = true; 
+
+        //Print the results//
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+ age);
+        System.out.println("GPA: "+gpa);
+        System.out.println("Full-Time: "+fullTime);
+
+    }
+}
