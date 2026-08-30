ead546f31bf9889c6e4c1af48d5f6ee7f2c26af1
Theophillus-Keitumetse <mk24019719@biust.ac.bw>
2026-08-30 19:53:00 +0200

    Q2 - Primitive Playground

---
+++ Question2.java
@@ -0,0 +1,24 @@
+public class Question2 {
+    public static void main(String[] args){
+
+        //Declaration of variables//
+       byte byteValue = 100;
+        short shortValue = 20000;
+        int intValue = 350000;
+        long longValue = 9000000000L;
+        float floatValue = 3.14f;
+        double doubleValue = 3.14159265;
+        char charValue = 'J';
+        boolean booleanValue = true;
+
+        //Printing of values//
+        System.out.println("byte: " + byteValue);
+        System.out.println("short: " + shortValue);
+        System.out.println("int: " + intValue);
+        System.out.println("long: " + longValue);
+        System.out.println("float: " + floatValue);
+        System.out.println("double: " + doubleValue);
+        System.out.println("char: " + charValue);
+        System.out.println("boolean: " + booleanValue);
+    }
+}
