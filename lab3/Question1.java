c2f46096c1ee9dd6e5742d79a272940659d93a22
Theophillus-Keitumetse <mk24019719@biust.ac.bw>
2026-09-13 20:15:16 +0200

    Add Question 1

---
+++ lab3/Question1.java
@@ -0,0 +1,20 @@
+public class Question1 {
+    public static void main(String[] args) {
+
+        // Start the countdown at 10
+        int number = 10;
+
+        // Keep looping while number is 1 or greater
+        while (number >= 1) {
+
+            // Print the current number
+            System.out.println(number);
+
+            // Decrease the number by 1
+            number--;
+        }
+
+        // This prints after the loop finishes
+        System.out.println("Liftoff!");
+    }
+}
