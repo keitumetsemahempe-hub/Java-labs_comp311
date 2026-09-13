df9d41735722fbac0bfb6cac6860569cf785fc43
Theophillus-Keitumetse <mk24019719@biust.ac.bw>
2026-09-13 20:28:29 +0200

    Add Question 3

---
+++ lab3/Question3.java
@@ -0,0 +1,17 @@
+public class Question3 {
+    public static void main(String[] args) {
+
+        // Start with the first even number
+        int number = 2;
+
+        // Keep looping while number is 50 or less
+        while (number <= 50) {
+
+            // Print the current even number
+            System.out.println(number);
+
+            // Move to the next even number
+            number = number + 2;
+        }
+    }
+}
